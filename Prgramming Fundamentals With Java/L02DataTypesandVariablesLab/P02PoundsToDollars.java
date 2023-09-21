b0ec79cc8983a51e36c618fbeb5da4143cf4c8e6
Tanya Stoyanova <145493604+tanyastoyanova87@users.noreply.github.com>
2023-09-21 16:48:44 +0300

    Changed problem Pound To Dollars

--- Prgramming Fundamentals With Java/L02DataTypesandVariablesLab/P02PoundsToDollars.java
+++ Prgramming Fundamentals With Java/L02DataTypesandVariablesLab/P02PoundsToDollars.java
@@ -6,7 +6,7 @@ public class P02PoundsToDollars {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int pounds = Integer.parseInt(scanner.nextLine());
+        double pounds = Double.parseDouble(scanner.nextLine());
 
         double poundsToDollars = pounds * 1.36;
         System.out.printf("%.3f%n", poundsToDollars);
