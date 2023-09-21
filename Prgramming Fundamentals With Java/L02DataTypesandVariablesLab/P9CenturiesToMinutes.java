b1454eaedb8eb34452e939e9f282b6ad8fb15c6f
Tanya Stoyanova <145493604+tanyastoyanova87@users.noreply.github.com>
2023-09-21 17:46:23 +0300

    Added new problem

---
+++ Prgramming Fundamentals With Java/L02DataTypesandVariablesLab/P9CenturiesToMinutes.java
@@ -0,0 +1,18 @@
+package L02DataTypesandVariablesLab;
+
+import java.util.Scanner;
+
+public class P9CenturiesToMinutes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int century = Integer.parseInt(scanner.nextLine());
+
+        double years = century * 100;
+        double days = years * 365.2422;
+        double hours = days * 24;
+        double minutes = hours * 60;
+
+        System.out.printf("%d centuries = %.0f years = %.0f days = %.0f hours = %.0f minutes%n", century, years, days, hours, minutes);
+    }
+}
