8c12b2338c88c7752725daf2bb6dfdb91273acaf
Tanya Stoyanova <145493604+tanyastoyanova87@users.noreply.github.com>
2023-09-24 21:53:00 +0300

    Added new problem "Sum Digits"

---
+++ Prgramming Fundamentals With Java/L02DataTypesandVariablesExercise/P02SumDigits.java
@@ -0,0 +1,22 @@
+package L02DataTypesandVariablesExercise;
+
+import java.util.Scanner;
+
+public class P02SumDigits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = Integer.parseInt(scanner.nextLine());
+
+        int sum = 0;
+
+        while (num > 0) {
+
+            int lastDigit = num % 10;
+            sum += lastDigit;
+            num = num / 10;
+
+        }
+            System.out.println(sum);
+    }
+}
