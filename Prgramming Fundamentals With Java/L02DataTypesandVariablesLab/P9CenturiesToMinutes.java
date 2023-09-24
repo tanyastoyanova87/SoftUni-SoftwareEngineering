59a18b37bd20c2b4d0f728e9870c46e739458c3a
Tanya Stoyanova <145493604+tanyastoyanova87@users.noreply.github.com>
2023-09-24 17:13:13 +0300

    Changed "Centuries To Minutes

--- Prgramming Fundamentals With Java/L02DataTypesandVariablesLab/P9CenturiesToMinutes.java
+++ Prgramming Fundamentals With Java/L02DataTypesandVariablesLab/P9CenturiesToMinutes.java
@@ -13,6 +13,11 @@ public class P9CenturiesToMinutes {
         double hours = days * 24;
         double minutes = hours * 60;
 
-        System.out.printf("%d centuries = %.0f years = %.0f days = %.0f hours = %.0f minutes%n", century, years, days, hours, minutes);
+        System.out.printf("%d centuries = %.0f years = %.0f days = %.0f hours = %.0f minutes%n",
+                century,
+                years,
+                days,
+                hours,
+                minutes);
     }
 }
