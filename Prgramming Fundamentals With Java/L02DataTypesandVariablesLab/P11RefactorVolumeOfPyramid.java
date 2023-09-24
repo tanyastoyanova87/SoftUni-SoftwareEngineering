d292435af7314bd55c4875893b73caa33e2b30b8
Tanya Stoyanova <145493604+tanyastoyanova87@users.noreply.github.com>
2023-09-24 19:16:02 +0300

    Added new problem "Refactor Volume Of Pyramid"

---
+++ Prgramming Fundamentals With Java/L02DataTypesandVariablesLab/P11RefactorVolumeOfPyramid.java
@@ -0,0 +1,19 @@
+package L02DataTypesandVariablesLab;
+
+import java.util.Scanner;
+
+public class P11RefactorVolumeOfPyramid {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double length = Double.parseDouble(scanner.nextLine());
+        System.out.print("Length: ");
+        double width = Double.parseDouble(scanner.nextLine());
+        System.out.print("Width: ");
+        double height = Double.parseDouble(scanner.nextLine());
+        System.out.print("Height: ");
+        double volume = (length * width * height) / 3;
+        System.out.printf("Pyramid Volume: %.2f%n", volume);
+
+    }
+}
