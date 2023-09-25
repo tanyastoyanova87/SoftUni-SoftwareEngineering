953ca3625cb0f2d5601324124cfb0a439abbc2ca
Tanya Stoyanova <145493604+tanyastoyanova87@users.noreply.github.com>
2023-09-25 15:57:31 +0300

    Added "Print Part Pf ASCII Table"

---
+++ Prgramming Fundamentals With Java/L02DataTypesandVariablesExercise/P05PrintPartOfASCIITable.java
@@ -0,0 +1,18 @@
+package L02DataTypesandVariablesExercise;
+
+import java.util.Scanner;
+
+public class P05PrintPartOfASCIITable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int start = Integer.parseInt(scanner.nextLine());
+        int end = Integer.parseInt(scanner.nextLine());
+
+        for (int i = start; i <= end ; i++) {
+
+            System.out.print((char) i + " ");
+        }
+
+    }
+}
