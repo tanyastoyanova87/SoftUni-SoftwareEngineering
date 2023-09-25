8c02ad5e663fdf2280db701cd3f8006019145dd6
Tanya Stoyanova <145493604+tanyastoyanova87@users.noreply.github.com>
2023-09-25 17:05:41 +0300

    Added "Water Overflow"

---
+++ Prgramming Fundamentals With Java/L02DataTypesandVariablesExercise/P07WaterOverflow.java
@@ -0,0 +1,24 @@
+package L02DataTypesandVariablesExercise;
+
+import java.util.Scanner;
+
+public class P07WaterOverflow {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int sum = 0;
+        for (int i = 1; i <= n ; i++) {
+
+            int litres = Integer.parseInt(scanner.nextLine());
+            sum += litres;
+
+            if (sum > 255){
+                System.out.println("Insufficient capacity!");
+                sum -= litres;
+            }
+        }
+        System.out.println(sum);
+    }
+}
