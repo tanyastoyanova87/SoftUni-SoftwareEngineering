f28901688d93dcaf39ff231fe64db16afc0e1b81
Tanya Stoyanova <145493604+tanyastoyanova87@users.noreply.github.com>
2023-09-29 17:32:03 +0300

    Added P3 "Sum Even Numbers"

---
+++ Programming Fundamentals With Java/L03ArraysLab/P03SumEvenNumbers.java
@@ -0,0 +1,25 @@
+package L03ArraysLab;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P03SumEvenNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = Arrays.stream(scanner.nextLine()
+                .split(" "))
+                .mapToInt(e -> Integer.parseInt(e))
+                .toArray();
+
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0){
+                sum += numbers[i];
+            }
+        }
+        System.out.println(sum);
+    }
+}
