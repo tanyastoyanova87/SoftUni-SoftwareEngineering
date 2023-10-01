c3c9cd8f0641a12a4d4ff35d17bb8637729901fc
Tanya Stoyanova <145493604+tanyastoyanova87@users.noreply.github.com>
2023-10-01 18:24:13 +0300

    Added P4 "Array Rotation"

---
+++ Programming Fundamentals With Java/L03ArraysExersice/P04ArrayRotation.java
@@ -0,0 +1,29 @@
+package L03ArraysExersice;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P04ArrayRotation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int [] numbers = Arrays.stream(scanner.nextLine()
+                .split(" "))
+                .mapToInt(e -> Integer.parseInt(e))
+                .toArray();
+
+        int rotations = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= rotations ; i++) {
+            int firstElement = numbers[0];
+
+            for (int index = 0; index < numbers.length - 1; index++) {
+                numbers[index] = numbers[index + 1];
+            }
+            numbers[numbers.length - 1] = firstElement;
+        }
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+    }
+}
