c0ab8628873bdfa6c1c06fab521d418e0149b8af
Tanya Stoyanova <145493604+tanyastoyanova87@users.noreply.github.com>
2023-10-02 23:24:46 +0300

    Added P6 "Equal Sums"

---
+++ Programming Fundamentals With Java/L03ArraysExersice/P06EqualSums.java
@@ -0,0 +1,29 @@
+package L03ArraysExersice;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P06EqualSums {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = Arrays.stream(scanner.nextLine()
+                .split(" "))
+                .mapToInt(e -> Integer.parseInt(e))
+                .toArray();
+
+        int numSum = Integer.parseInt(scanner.nextLine());
+
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            int currentNum = numbers[i];
+
+            for (int j = i + 1; j < numbers.length; j++) {
+                int num = numbers[j];
+                if (currentNum + num == numSum){
+                    System.out.println(currentNum + " " + num);
+                }
+            }
+        }
+    }
+}
