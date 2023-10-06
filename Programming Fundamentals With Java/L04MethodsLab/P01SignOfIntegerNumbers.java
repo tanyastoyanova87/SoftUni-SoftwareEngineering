44726c1fb2cf4cbd59bcb27319ae80b8488c8cf1
Tanya Stoyanova <145493604+tanyastoyanova87@users.noreply.github.com>
2023-10-06 20:53:03 +0300

    Added P1 " Sign of Integer Numbers"

---
+++ Programming Fundamentals With Java/L04MethodsLab/P01SignOfIntegerNumbers.java
@@ -0,0 +1,23 @@
+package L04MethodsLab;
+
+import java.util.Scanner;
+
+public class P01SignOfIntegerNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+        printSign(number);
+
+    }
+
+    public static void printSign(int number){
+        if (number == 0){
+            System.out.println("The number 0 is zero.");
+        }else if (number > 0){
+            System.out.printf("The number %d is positive.%n", number);
+        }else {
+            System.out.printf("The number %d is negative.%n", number);
+        }
+    }
+}
