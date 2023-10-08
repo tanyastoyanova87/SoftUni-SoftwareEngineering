167402bf3b5239815ca79863187e782074fa61ff
Tanya Stoyanova <145493604+tanyastoyanova87@users.noreply.github.com>
2023-10-08 16:50:34 +0300

    Reformat code in P4

--- Programming Fundamentals With Java/L04MethodsLab/P04Calculations.java
+++ Programming Fundamentals With Java/L04MethodsLab/P04Calculations.java
@@ -10,16 +10,16 @@ public class P04Calculations {
         int num1 = Integer.parseInt(scanner.nextLine());
         int num2 = Integer.parseInt(scanner.nextLine());
 
-        if (operation.equals("add")){
+        if (operation.equals("add")) {
             printCalculationAdd(num1, num2);
 
         } else if (operation.equals("multiply")) {
             printCalculationMultiply(num1, num2);
 
-        } else if (operation.equals("subtract")){
-            printCalculationSubtract(num1,num2);
+        } else if (operation.equals("subtract")) {
+            printCalculationSubtract(num1, num2);
 
-        } else if (operation.equals("divide")){
+        } else if (operation.equals("divide")) {
             printCalculationDivide(num1, num2);
         }
     }
@@ -28,16 +28,16 @@ public class P04Calculations {
         System.out.println(num1 + num2);
     }
 
-    public static void printCalculationMultiply(int num1, int num2){
+    public static void printCalculationMultiply(int num1, int num2) {
         System.out.println(num1 * num2);
     }
 
-    public static void printCalculationSubtract(int num1, int num2){
+    public static void printCalculationSubtract(int num1, int num2) {
         System.out.println(num1 - num2);
 
     }
 
-    public static void printCalculationDivide(int num1, int num2){
+    public static void printCalculationDivide(int num1, int num2) {
         System.out.println(num1 / num2);
     }
 }
