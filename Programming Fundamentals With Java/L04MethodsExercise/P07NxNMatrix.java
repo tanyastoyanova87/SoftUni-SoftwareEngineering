3e53255d7dba188abd293c126089d45c37d7505c
Tanya Stoyanova <145493604+tanyastoyanova87@users.noreply.github.com>
2023-10-09 21:10:13 +0300

    Added P7 "NxN Matrix"

---
+++ Programming Fundamentals With Java/L04MethodsExercise/P07NxNMatrix.java
@@ -0,0 +1,23 @@
+package L04MethodsExercise;
+
+import java.util.Scanner;
+
+public class P07NxNMatrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        printMatrix(n);
+    }
+
+    public static void printMatrix (int n) {
+
+        for (int i = 1; i <= n ; i++) {
+            for (int j = 1; j <= n ; j++) {
+                System.out.print(n + " ");
+            }
+            System.out.println();
+        }
+    }
+}
