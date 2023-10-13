b9754222b3ad4ff1be01d2649e235c24593b1549
Tanya Stoyanova <145493604+tanyastoyanova87@users.noreply.github.com>
2023-10-13 17:36:45 +0300

    Added P1 "Sum Adjacent Equal Numbers"

---
+++ Programming Fundamentals With Java/L05ListsLab/P01SumAdjacentEqualNumbers.java
@@ -0,0 +1,31 @@
+package L05ListsLab;
+
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class P01SumAdjacentEqualNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Double> numbers = Arrays.stream(scanner.nextLine()
+                        .split(" "))
+                .map(e -> Double.parseDouble(e))
+                .collect(Collectors.toList());
+
+
+        for (int i = 0; i < numbers.size() - 1; i++) {
+            if (numbers.get(i).equals(numbers.get(i + 1))) {
+                numbers.set(i, numbers.get(i) + numbers.get(i + 1));
+                numbers.remove(i + 1);
+                i =- 1;
+            }
+        }
+        for (double num : numbers) {
+            System.out.print(new DecimalFormat("0.#").format(num) + " ");
+        }
+        System.out.println();
+    }
+}
