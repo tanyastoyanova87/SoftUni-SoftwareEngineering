a5f831efa2e1b32ba6947ab68e23b7a6c480c15f
Tanya Stoyanova <145493604+tanyastoyanova87@users.noreply.github.com>
2023-11-01 18:17:44 +0200

    Added P4 "Word Filter"

---
+++ Programming Fundamentals With Java/L07MapsLambdaAndStreamAPILab/P04WordFilter.java
@@ -0,0 +1,18 @@
+package L07MapsLambdaAndStreamAPILab;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P04WordFilter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] words = Arrays.stream(scanner.nextLine().split(" "))
+                .filter(word -> word.length() % 2 == 0)
+                .toArray(String[]::new);
+
+        for (String word : words) {
+            System.out.println(word);
+        }
+    }
+}
