91009d8be2b01260ed5e78c7d2817e036fc5964b
Tanya Stoyanova <145493604+tanyastoyanova87@users.noreply.github.com>
2023-11-09 17:45:38 +0200

    Added P3 "Substring"

---
+++ Programming Fundamentals With Java/L08TextProcessingLab/P03Substring.java
@@ -0,0 +1,28 @@
+package L08TextProcessingLab;
+
+import java.util.Scanner;
+
+public class P03Substring {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String word = scanner.nextLine();
+        String text = scanner.nextLine();
+
+        //while (true) {
+        while (text.contains(word)) {
+            int index = text.indexOf(word);
+
+            //if (index == -1) {
+            //    break;
+            //}
+
+            String leftSubstring = text.substring(0, index);
+            String rightSubstring = text.substring(index + word.length());
+
+            text = leftSubstring + rightSubstring;
+        }
+
+        System.out.println(text);
+    }
+}
