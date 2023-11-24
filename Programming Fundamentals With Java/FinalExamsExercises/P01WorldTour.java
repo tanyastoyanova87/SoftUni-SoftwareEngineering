88af6b553a6d003912571972819b0781d6368b6c
Tanya Stoyanova <145493604+tanyastoyanova87@users.noreply.github.com>
2023-11-24 16:19:58 +0200

    Added P1 "World Tour"

---
+++ Programming Fundamentals With Java/FinalExamsExercises/P01WorldTour.java
@@ -0,0 +1,62 @@
+package FinalExamsExercises;
+
+import java.util.Scanner;
+
+public class P01WorldTour {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String stops = scanner.nextLine();
+
+        String commands = scanner.nextLine();
+        while (!commands.equals("Travel")) {
+
+            String command = commands.split(":")[0];
+
+            if (command.equals("Add Stop")) {
+                int index = Integer.parseInt(commands.split(":")[1]);
+                String textToAdd = commands.split(":")[2];
+
+                if (indexIsValid(index, stops)) {
+                    String left = stops.substring(0, index);
+                    String right = stops.substring(index);
+
+                    stops = left + textToAdd + right;
+                }
+
+                System.out.println(stops);
+
+            } else if (command.equals("Remove Stop")) {
+                int startIndex = Integer.parseInt(commands.split(":")[1]);
+                int endIndex = Integer.parseInt(commands.split(":")[2]);
+
+                if (indexIsValid(startIndex, stops) && indexIsValid(endIndex, stops)) {
+                    String left = stops.substring(0, startIndex);
+                    String right = stops.substring(endIndex + 1);
+
+                    stops = left + right;
+                }
+
+                System.out.println(stops);
+
+            } else if (command.equals("Switch")) {
+                String oldString = commands.split(":")[1];
+                String newString = commands.split(":")[2];
+
+                if (stops.contains(oldString)) {
+                    stops = stops.replace(oldString, newString);
+                }
+
+                System.out.println(stops);
+            }
+
+            commands = scanner.nextLine();
+        }
+
+        System.out.printf("Ready for world tour! Planned stops: %s%n", stops);
+    }
+
+    private static boolean indexIsValid(int index, String stops) {
+        return index >= 0 && index < stops.length();
+    }
+}
