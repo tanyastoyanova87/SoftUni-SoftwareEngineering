477f05b363838eb990e6c749ae414bbe69796f3b
Tanya Stoyanova <145493604+tanyastoyanova87@users.noreply.github.com>
2023-11-29 16:09:20 +0200

    Added P2 "Mirror Words"

---
+++ Programming Fundamentals With Java/FinalExams/P02MirrorWords.java
@@ -0,0 +1,47 @@
+package FinalExams;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class P02MirrorWords {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+
+        Pattern pattern = Pattern.compile("([@#])(?<firstWord>[A-Za-z]{3,})\\1\\1(?<secondWord>[a-zA-Z]{3,})\\1");
+        Matcher matcher = pattern.matcher(text);
+
+        List<String> matches = new ArrayList<>();
+
+        int validPairs = 0;
+        while (matcher.find()) {
+            validPairs++;
+            String firstWord = matcher.group("firstWord");
+            String secondWord = matcher.group("secondWord");
+            StringBuilder secondWordBuilder = new StringBuilder(secondWord);
+            String reversedWord = String.valueOf(secondWordBuilder.reverse());
+
+            if (firstWord.equals(reversedWord)) {
+                matches.add(String.join(" <=> ", firstWord, secondWord));
+            }
+        }
+
+        if (validPairs > 0) {
+            System.out.printf("%d word pairs found!%n", validPairs);
+        } else {
+            System.out.println("No word pairs found!");
+        }
+
+        if (matches.isEmpty()) {
+            System.out.println("No mirror words!");
+        } else {
+            System.out.println("The mirror words are:");
+            System.out.println(String.join(", ", matches));
+
+        }
+    }
+}
